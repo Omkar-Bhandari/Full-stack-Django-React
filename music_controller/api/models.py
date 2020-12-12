c68dd852f95481a5846ad30d11dcd904590e8990
Omkar-Bhandari <68837275+Omkar-Bhandari@users.noreply.github.com>
2020-12-12 18:24:07 +0530

    new

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -5,7 +5,7 @@ import string
 def generate_unique_code():
     length = 6
     while True:
-        code = "".join(random.choices(string.ascii_uppercase))
+        code = "".join(random.choices(string.ascii_uppercase , k=length))
         if Room.objects.filter(code=code).count()==0:
             break
         
@@ -14,9 +14,9 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=10,default="",unique=True)
+    code = models.CharField(max_length=10,default=generate_unique_code,unique=True)
     host = models.CharField(max_length=50,null=False,unique=True)
     guest_can_pause = models.BooleanField(null=False,default=False)
-    votes_to_skip = models.IntegerField(null=False,default=2)
+    votes_to_skip = models.IntegerField(null=False,default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,30 @@
 import React, { Component} from 'react';
+import Button from "material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import FormHelperText from "material-ui/core/FormHelperText";
+import FormControl from "material-ui/core/FormControl";
+import TextField from "material-ui/core/TextField";
+import {Link} from "material-ui/core/Link";
+import Radio from "material-ui/core/Radio";
+import RadioGroup from "material-ui/core/RadioGroup";
+import FormControlLabel from "material-ui/core/FormControlLabel";
+import Checkbox from "material-ui/core/Checkbox";
 
 export default class CreateRoomPage extends Component{
+    
     constructor(props){
         super(props);
     }
 
     render(){
-        return <p><h1>This is the Room Create Page </h1></p>
+        return(
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center"></Grid>
+            <Typography component='h4' variant='h4'>
+            Create a Room
+            </Typography>
+        </Grid>
+        );
     }
 }
